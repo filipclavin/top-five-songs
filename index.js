cb33cb0f6d43628e2c89bdf24cd6ee0803ecd8e9
filipclavin <filip.clavin@student.kyh.se>
2020-10-17 18:03:01 +0200

    Validated HTML, CSS and JS

--- index.js
+++ index.js
@@ -1,76 +1,76 @@
 /* Fades elements in as they eneter the viewport. Based on https://www.dev-tips-and-tricks.com/animate-elements-scrolled-view-vanilla-js */
 
 (function() {
-    var elements
-    var windowHeight
+    var elements;
+    var windowHeight;
   
     function init() {
-      elements = document.querySelectorAll('.hidden')
-      windowHeight = window.innerHeight
+      elements = document.querySelectorAll('.hidden');
+      windowHeight = window.innerHeight;
     }
   
     function checkPosition() {
       for (var i = 0; i < elements.length; i++) {
-        var element = elements[i]
-        var positionFromTop = elements[i].getBoundingClientRect().top
-        var positionFromBottom = elements[i].getBoundingClientRect().bottom
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+        var positionFromBottom = elements[i].getBoundingClientRect().bottom;
   
         if (positionFromTop <= windowHeight) {
-          element.classList.add("fade-in-element")
-          element.classList.remove("hidden")
+          element.classList.add("fade-in-element");
+          element.classList.remove("hidden");
         } else if (positionFromTop > windowHeight) {
-            element.classList.add("hidden")
-            element.classList.remove("fade-in-element" || positionFromBottom <= 0)
+            element.classList.add("hidden");
+            element.classList.remove("fade-in-element" || positionFromBottom <= 0);
         }
       }
     }
+    
+    window.addEventListener("scroll", checkPosition);
+    window.addEventListener("resize", init);
   
-    window.addEventListener("scroll", checkPosition)
-    window.addEventListener("resize", init)
-  
-    init()
-    checkPosition()
-})()
+    init();
+    checkPosition();
+})();
   
 
 /* Form validation, based on https://www.youtube.com/watch?v=In0nB0ABaUk */
 
-const name = document.getElementById("name")
-const mail = document.getElementById("mail")
-const message = document.getElementById("message")
-const form = document.getElementById("form")
-const errorElement = document.getElementById("error")
+let name = document.getElementById("name");
+let mail = document.getElementById("mail");
+let message = document.getElementById("message");
+let form = document.getElementById("form");
+let errorElement = document.getElementById("error");
 
 form.addEventListener("submit", (e) => {
-    let errors = []
-    if (name.value === "" || name.value == null) {
-        errors.push("<p id=\'name-error\'>Please enter a name.</p>")
-        name.classList.add("bad-input")
+    let errors = [];
+    if (name.value === "" || name.value === null) {
+        errors.push("<p id=\'name-error\'>Please enter a name.</p>");
+        name.classList.add("bad-input");
     } else {
-        name.classList.remove("bad-input")
+        name.classList.remove("bad-input");
     }
 
-    if (mail.value === "" || mail.value == null) {
-        errors.push("<p id=\'mail-error\'>Please enter an e-mail address.</p>")
-        mail.classList.add("bad-input")
+    if (mail.value === "" || mail.value === null) {
+        errors.push("<p id=\'mail-error\'>Please enter an e-mail address.</p>");
+        mail.classList.add("bad-input");
     } else if (mail.validity.typeMismatch) {
-        errors.push("<p id=\'mail-error\'>You have not entered a valid e-mail adress.</p>")
-        mail.classList.add("bad-input")
+        errors.push("<p id=\'mail-error\'>You have not entered a valid e-mail adress.</p>");
+        mail.classList.add("bad-input");
     } else {
-        mail.classList.remove("bad-input")
+        mail.classList.remove("bad-input");
     }
 
-    if (message.value === "" || message.value == null) {
-        errors.push("<p id=\'message-error\'>Please enter a message.</p>")
-        message.classList.add("bad-input")
+    if (message.value === "" || message.value === null) {
+        errors.push("<p id=\'message-error\'>Please enter a message.</p>");
+        message.classList.add("bad-input");
     } else {
-        message.classList.remove("bad-input")
+        message.classList.remove("bad-input");
     }
 
     if (errors.length > 0) {
-        e.preventDefault()
-        errorElement.classList.add("active")
-        errorElement.innerHTML = errors.join("")
+        e.preventDefault();
+        errorElement.classList.add("active");
+        errorElement.innerHTML = errors.join("");
     }
-})
+});
 
